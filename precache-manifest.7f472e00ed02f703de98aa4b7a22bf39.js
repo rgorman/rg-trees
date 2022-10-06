68bbae18efe10fa78d31eed69e3f9452bd2021fb
rgorman <rgorman1@unl.edu>
2022-10-06 21:10:12 +0000

    Deploying to gh-pages from  @ bf556fe715371b3d1c7edcc7292eff37cef1fcb2 🚀

--- precache-manifest.7f472e00ed02f703de98aa4b7a22bf39.js
+++ precache-manifest.7f472e00ed02f703de98aa4b7a22bf39.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9ec1cd5a197064d0349e4f9702d8d47c",
+    "revision": "1cc0b10c18f58efb2c2f005f59598f4e",
     "url": "/rg-trees/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/rg-trees/static/css/2.4d5b04cb.chunk.css"
   },
   {
-    "revision": "6a56fe8e0b3137753c18",
+    "revision": "97cfec07eda5b3eb9f97",
     "url": "/rg-trees/static/css/main.bd98509e.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/rg-trees/static/js/2.557c03a8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6a56fe8e0b3137753c18",
-    "url": "/rg-trees/static/js/main.f47e2310.chunk.js"
+    "revision": "97cfec07eda5b3eb9f97",
+    "url": "/rg-trees/static/js/main.081fc29b.chunk.js"
   },
   {
     "revision": "2ce0ed2071b0663bfb3d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rg-trees/precache-manifest.314c1736ad4483a9e18fa8688e18dd90.js"
+  "/rg-trees/precache-manifest.7f472e00ed02f703de98aa4b7a22bf39.js"
 );
 
 self.addEventListener('message', (event) => {
